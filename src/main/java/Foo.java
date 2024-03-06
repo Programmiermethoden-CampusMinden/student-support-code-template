4947d9977664f7738f0a67c452b25d2710b8a8af
Carsten Gips <cagix@hsbi.de>
2024-03-06 12:02:42 +0100

    add simple java files for testing purposes

---
+++ src/main/java/Foo.java
@@ -0,0 +1,11 @@
+/** Foo. */
+public class Foo {
+    /**
+     * Foo#bar.
+     *
+     * @return a silly string
+     */
+    public static String bar() {
+        return "Foo{}";
+    }
+}
---
+++ src/test/java/FooTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FooTest {
+    @Test
+    void testBar() {
+        assertEquals(Foo.bar(), "Foo{}");
+    }
+}
