852bc7e515d0c15c2432e009cfdf286b7bc13007
Carsten Gips <cagix@hsbi.de>
2024-03-08 10:48:19 +0100

    add a dummy 'solution' file

---
+++ solution/main/java/Bar.java
@@ -0,0 +1,11 @@
+/** Bar. */
+public class Bar extends Foo {
+    /**
+     * Answer to life and everything.
+     *
+     * @return 42
+     */
+    public int answer() {
+        return 42;
+    }
+}
